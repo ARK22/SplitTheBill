ca610ac8cb3d90b6cab40237e662f513a812a9f6
mdn97 <mdn12@students.uwf.edu>
2019-12-01 23:15:52 -0600

    Made HTML modifications and added basic budget insertion

---
+++ newbudget.php
@@ -0,0 +1,29 @@
+<?php
+$budgetname = "";
+$itemname= "";
+$category = "";
+
+session_start();
+
+require 'connect.php';
+
+if (isset($_POST['createbudget'])) {
+	$budgetname = strip_tags(trim(mysqli_real_escape_string($conn, $_POST["budgetname"])));
+	$ownerid = $_SESSION['id'];
+	
+	$sqlNewBudget = "INSERT INTO Budgets (BudgetName, OwnerID) VALUES (?,?)";
+	$stmtNewBudget= $conn->prepare($sqlNewBudget);
+	$stmtNewBudget->bind_param('ss', $budgetname, $ownerid);
+	
+	$result = $stmtNewBudget->execute();
+	
+	if ($result) {
+		header("location: login.html");
+	echo $userid;
+	}
+	else {
+		echo "failure";
+		header("location:fail");
+	}
+}
+
