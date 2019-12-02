f5a8b85d4f74405354a1dcced0f75efa0d4d3a22
mdn97 <mdn12@students.uwf.edu>
2019-12-01 23:51:52 -0600

    Attempted insertion of budgetmember info in newbudget.php

--- newbudget.php
+++ newbudget.php
@@ -13,16 +13,34 @@ if (isset($_POST['createbudget'])) {
 	
 	$sqlNewBudget = "INSERT INTO Budgets (BudgetName, OwnerID) VALUES (?,?)";
 	$stmtNewBudget= $conn->prepare($sqlNewBudget);
-	$stmtNewBudget->bind_param('ss', $budgetname, $ownerid);
+	$stmtNewBudget->bind_param('si', $budgetname, $ownerid);
 	
 	$result = $stmtNewBudget->execute();
 	
 	if ($result) {
 		header("location: home.html");
-	echo $userid;
 	}
 	else {
 		header("location:fail");
 	}
-}
 
+	$sqlGetBudgetID = "SELECT BudgetID FROM Budgets WHERE OwnerID = ?";
+	$stmtGetBudgetID = $conn->prepare($sqlGetBudgetID);
+	$stmtGetBudgetID->bind_param('i', $ownerid);
+	$stmtGetBudgetID ->execute();
+	
+	//having issues with this part right here, i think
+	if ($stmtFindUsername->execute())
+	{
+		mysqli_store_result($stmtFindUsername);
+	}
+	echo $stmtFindUsername;
+	$memberid = $ownerid;
+
+	$ismoderator = 1; //for current user logged in
+	$sqlAddBudgetMember = "INSERT INTO BudgetMembers (MemberID, BudgetID, IsModerator) VALUES (?, ?, ?)";
+	$stmtAddBudgetMember = $conn->prepare($sqlAddBudgetMember);
+	$stmtAddBudgetMember->bind_param('iii', $memberid, $stmtFindUsername, $ismoderator);
+	
+}
+	?>
